228649b81f833ccf185fafe895068667847a8310
Flyfish <1611240037@qq.com>
2026-09-03 14:13:45 +0800

    [project_todos] 保证收到退出信号优雅退出，不接受新的请求且处理旧的请求

--- 002/project_todos/main.go
+++ 002/project_todos/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"project_todos/controllers"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,6 +32,29 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
-	r := setupRouter()
-	r.Run(":8080")
+	router := setupRouter()
+
+	svr := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := svr.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("正在关闭服务器...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := svr.Shutdown(ctx); err != nil {
+		log.Fatal("服务器强制关闭: ", err)
+	}
+
+	log.Println("服务器优雅关闭")
 }
